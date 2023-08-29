d5c747d44c57514f505299fc87752dee58dc11d7
Andrey <gusev2097@mail.ru>
2023-08-29 21:36:07 +0500

    Resolve tasks

--- Program.cs
+++ Program.cs
@@ -33,3 +33,36 @@ int CountNumUser ()
 }
 
 System.Console.WriteLine($"Количество элементов больше нуля - {CountNumUser()}");
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int Prompt(string msg)
+{
+    Console.Write($"{msg}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+double FindX (double k1, double b1, double k2, double b2)
+{
+    double x = (b1 - b2) / (-1 * (k1 - k2));
+    return x;
+}
+
+double FindY (double x, double k2, double b2)
+{
+    double y = k2 * x + b2;
+    return y;
+}
+
+double k1 = Prompt("Введите к1");
+double b1 = Prompt("Введите b1");
+double k2 = Prompt("Введите к2");
+double b2 = Prompt("Введите b2");
+
+double x = Math.Round(FindX(k1, b1, k2, b2), 1);
+double y = Math.Round(FindY(x, k2, b2), 1);
+
+Console.WriteLine($"X = {x}, Y = {y}");
